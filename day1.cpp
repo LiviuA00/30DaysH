a185dbbb5352880ec914f781a553a95bb55fe36b
LiviuA00 <liviuanton418@yahoo.com>
2024-06-11 15:36:09 +0300

    Day 1

---
+++ day1.cpp
@@ -0,0 +1,18 @@
+    int a;
+    double d2;
+    string c;
+
+    cin >> a;
+    cin >> d2;
+    while (cin) {
+        cin.get();
+        getline(cin, c);
+    }
+
+    cout << i + a << endl;
+
+    cout << fixed;
+    cout << setprecision(1);
+    cout << d2 + d << endl;
+
+    cout << s + c << endl;
